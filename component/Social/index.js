5ec00685c6bde933e1f64cd93daf209e99b3f07f
DavidWaza <wazabanga00@gmail.com>
2022-11-10 14:21:25 +0100

    added back the png file

--- component/Social/index.js
+++ component/Social/index.js
@@ -3,37 +3,39 @@ import styles from "../../styles/Home.module.css";
 import Connected from "/public/img/connected.png";
 import Image from "next/image";
 import Link from "next/link";
-
+import Insta from "../../public/img/insta.png";
 
 const SocialHandle = () => {
   return (
     <>
-    <Container fluid>
-      <Row className={` g-0 ${styles.colWrapper}`}>
-        <Col sm={6}>
-          <div className={styles.col1}></div>
-        </Col>
-        <Col sm={6}>
-          <div className={styles.col2}>
-            <div className={styles.socialIcon}>
-              <Image
-                src={Connected}
-                alt="IG"
-                className={styles.stayConnected}
-              />
+      <Container fluid>
+        <Row className={` g-0 ${styles.colWrapper}`}>
+          <Col sm={6}>
+            <div className={styles.col1}></div>
+          </Col>
+          <Col sm={6}>
+            <div className={styles.col2}>
+              <div className={styles.socialIcon}>
+                <Image
+                  src={Connected}
+                  alt="IG"
+                  className={styles.stayConnected}
+                />
+              </div>
+              <div className={styles.connectText}>
+                <p>Get Connected</p>
+                <p>@wazabanga.production</p>
+                <p>
+                  Follow us on Instagram to keep up to date with the latest
+                  news, promotions, giveaways and so much more.
+                </p>
+                <Link href="/">
+                  <Image src={Insta} alt="insta" />
+                </Link>
+              </div>
             </div>
-            <div className={styles.connectText}>
-              <p>Get Connected</p>
-              <p>@wazabanga.production</p>
-              <p>
-                Follow us on Instagram to keep up to date with the latest news,
-                promotions, giveaways and so much more.
-              </p>
-              
-            </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
       </Container>
     </>
   );
