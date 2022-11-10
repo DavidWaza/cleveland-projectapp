bfb754e15fe938bbde2a5724d7f1265a90883ea6
DavidWaza <wazabanga00@gmail.com>
2022-11-10 13:04:21 +0100

    re edited the social handle

--- component/Social/index.js
+++ component/Social/index.js
@@ -4,8 +4,7 @@ import Connected from "/public/img/connected.png";
 import Image from "next/image";
 import Link from "next/link";
 
-const VideoSocials =
-  "https://cdn.pixabay.com/vimeo/229221023/Jewelry%20-%2011289.mp4?width=1280&expiry=1668019411&hash=1dbde7f6a1deb1bbbaf11596e4ed4e397fa36168";
+
 const SocialHandle = () => {
   return (
     <>
