09a89fb86edb41fcfd6be9eda46d84003ee38ebc
DavidWaza <wazabanga00@gmail.com>
2022-11-13 00:31:37 +0100

    mapped through the crystals

---
+++ component/data/gemData.js
@@ -0,0 +1,21 @@
+
+
+export const Amethyst = [
+
+    {
+        name: "amethyst",
+        id: 1,
+        alt:"amethyst",
+        img: "/amethyst.jpg"
+    },
+    {
+        id: 2,
+        alt: "amethyst",
+        img: "/amethyst2.jpg",
+    },
+    {
+        id: 3,
+        alt: "amethyst",
+        img: "/amethyst3.jpg",
+    },
+]
--- pages/rawgems.js
+++ pages/rawgems.js
@@ -1,14 +1,17 @@
+import Image from "next/image";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import styles from "../styles/Raw.module.css";
+import Rawstyles from "../styles/Raw.module.css";
+import styles from "../styles/Home.module.css";
+import { Amethyst } from "../component/data/gemData";
 
 const Raw = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Container>
         <Row className="g-0">
-          <Col sm={8} className={styles.wrapper}>
-            <div className={styles.rawDiv}>
+          <Col sm={8} className={Rawstyles.wrapper}>
+            <div className={Rawstyles.rawDiv}>
               <p>Raw Collection</p>
               <p>
                 We journey through different part of the world to carefully
@@ -17,8 +20,20 @@ const Raw = () => {
             </div>
           </Col>
           <Col sm={4}>
-            <div className={styles.rawImgHole}></div>
+            <div className={Rawstyles.rawImgHole}></div>
           </Col>
+        </Row>
+        <Row className={Rawstyles.row}>
+          <div className={Rawstyles.gemProps}>
+            <p>amethyst</p>
+          </div>
+          {Amethyst.map((gem) => (
+            <>
+              <Col sm={4} key={gem.id}>
+                <Image src={gem.img} alt={gem.alt} width={350} height={300} />
+              </Col>
+            </>
+          ))}
         </Row>
       </Container>
     </div>
